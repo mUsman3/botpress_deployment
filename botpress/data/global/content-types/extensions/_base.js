c7cd7016b340ec8a1993755b0f307ffbab797fad
musman3 <usman59228@gmail.com>
2021-08-04 08:39:32 +0000

    updated docker-compose file

---
+++ botpress/data/global/content-types/extensions/_base.js
@@ -0,0 +1,21 @@
+//CHECKSUM:e37985aa65b16c277e72bf6c19e688906b960159cc0a7f0b62984fb3b44545d4
+module.exports = {
+  typingIndicators: {
+    typing: {
+      type: 'boolean',
+      title: 'Show typing indicators',
+      default: true
+    }
+  },
+  useMarkdown: {
+    markdown: {
+      type: 'boolean',
+      title: 'module.builtin.useMarkdown',
+      default: true,
+      $help: {
+        text: 'module.builtin.markdownHelp',
+        link: 'https://daringfireball.net/projects/markdown/syntax'
+      }
+    }
+  }
+}
